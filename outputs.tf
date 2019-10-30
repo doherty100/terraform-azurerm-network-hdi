fd5ad25349db4cca921cf941c90b3bb6f64ba287
tim.brigley@rubikloud.com <tim.brigley@rubikloud.com>
2019-10-30 15:14:06 -0400

    outputs added. Ref: https://www.terraform.io/docs/providers/azurerm/r/hdinsight_spark_cluster.html

---
+++ outputs.tf
@@ -0,0 +1,20 @@
+output "subnet_id" {
+  value = azurerm_subnet.subnet.id
+}
+
+output "virtual_network_id" {
+  value = azurerm_virtual_network.vnet.id
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.*.name
+}
+
+output "storage_account_key" {
+  value = azurerm_storage_account.storage.*.primary_access_key
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
